d9a77bfed2afabbe922fb12c0ca57648a3f545ed
Ренета Бонева <reneta.boneva@abv.bg>
2024-11-03 21:49:05 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { LessonPlan } from './components/LessonPlan';
 
 function App() {
   return (
     <>
-    <h1>Lesson plan with AI</h1>
+      <h1>Lesson plan with AI</h1>
+      <Routes>
+        <Route path='*' element={<LessonPlan />} />
+      </Routes>
     </>
   );
 }
