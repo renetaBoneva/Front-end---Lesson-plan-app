cebac0e72eecd641350800cab946d67195ee0575
Ренета Бонева <reneta.boneva@abv.bg>
2025-01-02 17:40:43 +0200

    Working on error messages and adding notes and type fields

--- src/components/LessonPlan.js
+++ src/components/LessonPlan.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import * as lessonPlanService from "../services/lessonPlanService";
+
+// да добавя поле за препоръки
+// да добавя дали урокът е за нови знания или за упражнение 
 
 export function LessonPlan() {
     const [values, setValues] = useState({
         "class": "",
         "course": "",
         "theme": "",
+        "type": "",
         "time": "40",
         "method": true,
         "target": true,
@@ -15,12 +20,12 @@ export function LessonPlan() {
         "mainPart": true,
         "exercises": true,
         "conclusion": true,
-        "homework": true
+        "homework": true,
+        "notes": ""
     });
-    const [errors, setErrors] = useState({
-
-    })
-
+    const [errors, setErrors] = useState({})
+    const [lessonPlan, setLessonPlan] = useState("");
+    const [isLoading, seIsLoading] = useState(false)
 
     function handleChange(e) {
         // Change Values;
@@ -31,6 +36,7 @@ export function LessonPlan() {
             [name]: type === 'checkbox' ? checked : value
         }));
 
+        //Validate some values
         handleIsValid(e);
     }
 
@@ -52,16 +58,39 @@ export function LessonPlan() {
         } else {
             setErrors(state => ({ ...state, "theme": "" }))
         }
+
+        if (values.type === "") {
+            setErrors(state => ({ ...state, "type": "Въведете тип на урока!" }))
+        } else {
+            setErrors(state => ({ ...state, "type": "" }))
+        }
     }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!(errors.course || errors.class || errors.theme || errors.type)) {
+            seIsLoading(true);
+            try {
+                lessonPlanService.generateMeLessonPlan(values)
+                    .then(res => {
+                        const generatedPlan = res.aiResponse;
+
+                        setLessonPlan(generatedPlan)
+                    })
+            }
+            catch (err) {
+                console.log(err);
+            }
+            seIsLoading(false);
+        }
+    }
+
     return (
         <>
             <h2>Generate lecture plan</h2>
-            <form>
-                {
-                    errors.class
-                        ? <p className="errP">{errors.class}</p>
-                        : ""
-                }
+            <div className="lessonPlanWrapper"></div>
+            <form onSubmit={handleSubmit}>
+                {errors.class && <p className="errP">{errors.class}</p>}
                 <label htmlFor="class">Клас*</label>
                 <select name="class"
                     value={values.class}
@@ -81,11 +110,7 @@ export function LessonPlan() {
                     <option>12</option>
                 </select>
 
-                {
-                    errors.course
-                        ? <p className="errP">{errors.course}</p>
-                        : ""
-                }
+                {errors.course && <p className="errP">{errors.course}</p>}
                 <label htmlFor="course">Предмет*</label>
                 <input
                     type="text"
@@ -95,11 +120,7 @@ export function LessonPlan() {
                     onBlur={handleIsValid}
                 />
 
-                {
-                    errors.theme
-                        ? <p className="errP">{errors.theme}</p>
-                        : ""
-                }
+                {errors.theme && <p className="errP">{errors.theme}</p>}
                 <label htmlFor="theme">Тема на урок*</label>
                 <input
                     type="text"
@@ -108,6 +129,20 @@ export function LessonPlan() {
                     onChange={handleChange}
                     onBlur={handleIsValid}
                 />
+
+
+                {errors.type && <p className="errP">{errors.type}</p>}
+                <label htmlFor="type">Тип на урока</label>
+                <select
+                    name="type"
+                    value={values.type}
+                    onChange={handleChange}
+                    onBlur={handleIsValid}
+                >
+                    <option>За нови знания</option>
+                    <option>За упражнение</option>
+                </select>
+
                 <label htmlFor="time">Продължителност</label>
                 <select
                     name="time"
@@ -120,6 +155,7 @@ export function LessonPlan() {
                     <option>45</option>
                 </select>
 
+                {/* Да се включи в плана: */}
                 <fieldset>
                     <legend>Да се включи в плана:</legend>
                     <div className="LesPlanCheckBoxWrapper">
@@ -173,6 +209,7 @@ export function LessonPlan() {
                     </div>
                 </fieldset>
 
+                {/* Структура и ход на урока: */}
                 <fieldset>
                     <legend>Структура и ход на урока:</legend>
                     <div className="LesPlanCheckBoxWrapper">
@@ -216,9 +253,19 @@ export function LessonPlan() {
                     </div>
                 </fieldset>
 
-                <button className="submitBtn" type="submit">
+                <label htmlFor="notes">Бележки:</label>
+                <textarea
+                    name="notes"
+                    value={values.notes}
+                    onChange={handleChange} />
+
+                <button className="submitBtn" type="submit" disabled={errors.class || errors.course || errors.theme}>
                     Генерирай
                 </button>
             </form>
+            {isLoading
+                ? <p>Loading ...</p>
+                : lessonPlan && <p id="LessonPlanP"> {lessonPlan} </p>
+            }
         </>);
 }
