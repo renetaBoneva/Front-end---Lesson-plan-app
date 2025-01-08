de15723c4d20f4a98fae184eae0ba6475cf192a1
Ренета Бонева <reneta.boneva@abv.bg>
2025-01-08 17:52:48 +0200

    Added download as pdf button

--- src/components/LessonPlan/TextEditor/TextEditor.js
+++ src/components/LessonPlan/TextEditor/TextEditor.js
@@ -1,17 +1,37 @@
-import { EditorState, ContentState } from "draft-js";
 import { Component } from "react";
 import { Editor } from "react-draft-wysiwyg";
+import { EditorState, ContentState, convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import html2pdf from "html2pdf.js/dist/html2pdf.bundle.min.js";
+import './TextEditor.css';
 
+
+// TODO: Remove disfunctional buttons
 export default class TextEditor extends Component {
-    constructor(props) {
-        super(props);
+    state = {
+        editorState: EditorState.createEmpty(),
+    };
 
-        this.state = {
-            editorState: props.lessonPlan
-                ? EditorState.createWithContent(ContentState.createFromText(props.lessonPlan))
-                : EditorState.createEmpty(),
-        };
+    componentDidMount() {
+        if (this.props.lessonPlan) {
+            this.setState({
+                editorState: EditorState.createWithContent(
+                    ContentState.createFromText(this.props.lessonPlan)
+                ),
+            });
+        }
+    }
+
+    // Update editorState if lessonPlan prop changes
+    componentDidUpdate(prevProps) {
+        if (prevProps.lessonPlan !== this.props.lessonPlan) {
+            this.setState({
+                editorState: this.props.lessonPlan
+                    ? EditorState.createWithContent(ContentState.createFromText(this.props.lessonPlan))
+                    : EditorState.createEmpty(),
+            });
+        }
     }
 
     onEditorStateChange = (editorState) => {
@@ -20,10 +40,65 @@ export default class TextEditor extends Component {
         })
     }
 
-    render() { 
-        const {editorState} = this.state;
+    saveAsPDF = () => {
+        const { editorState } = this.state;
+        const rawContentState = convertToRaw(editorState.getCurrentContent());
+        const htmlContent = draftToHtml(rawContentState);
 
-        return(
+        const options = {
+            margin: 1,
+            filename: "LessonPlan.pdf",
+            image: { type: "jpeg", quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: "in", format: "A4", orientation: "portrait" },
+        };
+
+        html2pdf().from(htmlContent).set(options).save();
+    };
+
+    // Working for .doc
+    saveAsDOC = () => {
+        const { editorState } = this.state;
+        const rawContentState = convertToRaw(editorState.getCurrentContent());
+        const htmlContent = draftToHtml(rawContentState);
+
+        var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+        var postHtml = "</body></html>";
+        var html = preHtml + htmlContent + postHtml;
+
+        var blob = new Blob(['\ufeff', html], {
+            type: 'application/msword'
+        });
+
+        // Specify link url
+        var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+
+        // Create download link element
+        var downloadLink = document.createElement("a");
+
+        document.body.appendChild(downloadLink);
+
+        if (navigator.msSaveOrOpenBlob) {
+            navigator.msSaveOrOpenBlob(blob, 'LessonPlan.doc');
+        } else {
+            // Create a link to the file
+            downloadLink.href = url;
+
+            // Setting the file name
+            downloadLink.download = 'LessonPlan.doc';
+
+            //triggering the function
+            downloadLink.click();
+        }
+
+        document.body.removeChild(downloadLink);
+
+    };
+
+    render() {
+        const { editorState } = this.state;
+
+        return (<>
             <Editor
                 editorState={editorState}
                 toolbarClassName="toolbarClassName"
@@ -31,6 +106,11 @@ export default class TextEditor extends Component {
                 editorClassName="editorClassName"
                 onEditorStateChange={this.onEditorStateChange}
             />
+
+            {/* Save as PDF Button */}
+            <button onClick={this.saveAsPDF}>Save as PDF</button>
+            {/* <button onClick={this.saveAsDOC}>Save as DOC</button> */}
+        </>
         );
     }
 }
