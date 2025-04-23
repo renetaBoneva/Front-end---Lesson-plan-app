e5b094172c6227d2c6ecf81b48097cdee96d6eeb
Ренета Бонева <reneta.boneva@abv.bg>
2025-04-23 16:55:00 +0300

    Control data input with React in login page

--- src/components/User/Login/Login.js
+++ src/components/User/Login/Login.js
@@ -1,37 +1,93 @@
+import { useState } from 'react';
 import './Login.css';
 
 export function Login() {
+    const [values, setValues] = useState({
+        "email": "",
+        "password": "",
+    });
+    const [errors, setErrors] = useState({});
+    const [isTouched, setIsTouched] = useState(false);
+
+    function handleChange(e) {
+        // Change Values;
+        const { name, value } = e.target;
+
+        setValues(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+
+        //Validate some values
+        handleIsValid(e);       
+    }
+
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        if (!(errors.password || errors.email)) {
+            // setIsLoading(true);
+
+            try {
+                // const res = await lessonPlanService.generateMeLessonPlan(values);
+                // const generatedPlan = res.aiResponse;
+                // setLessonPlan(generatedPlan);
+                console.log(values);
+
+            } catch (err) {
+                console.log(err);
+            } finally {
+                // setIsLoading(false);
+                setIsTouched(false);
+            }
+        }
+    }
+
+    function handleIsValid(e) {
+        setIsTouched(true);
+
+        if (values.email === "") {
+            setErrors(state => ({ ...state, "email": "Въведете имейл!" }))
+        } else {
+            setErrors(state => ({ ...state, "email": "" }))
+        }
+        
+        if (values.password === "") {
+            setErrors(state => ({ ...state, "password": "Въведете парола!" }))
+        } else {
+            setErrors(state => ({ ...state, "password": "" }))
+        }
+    }
+
+
     return (
         <div className="wrapper">
-            <form id='loginForm'>
+            <form id='loginForm' onSubmit={handleSubmit}>
                 <h2>Вход</h2>
 
                 <label htmlFor="email">Имейл<span className="requiredStart">*</span></label>
                 <input
                     type="email"
                     name="email"
-                // value={values.theme}
-                // onChange={handleChange}
-                // onBlur={handleIsValid}
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleIsValid}
                 />
-                {/* {errors.theme && <p className="errP">{errors.theme}</p>} */}
+                {errors.email && <p className="errP">{errors.email}</p>}
 
 
                 <label htmlFor="password">Парола<span className="requiredStart">*</span></label>
                 <input
                     type="password"
                     name="password"
-                // value={values.theme}
-                // onChange={handleChange}
-                // onBlur={handleIsValid}
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleIsValid}
                 />
-                {/* {errors.theme && <p className="errP">{errors.theme}</p>} */}
-
-
+                {errors.password && <p className="errP">{errors.password}</p>}
 
                 <button className="submitBtn btn2" type="submit"
-                // disabled={!isTouched || (errors.class || errors.course || errors.theme)}
-                >
+                    disabled={!isTouched || (errors.email || errors.password)}                >
                     Вход
                 </button>
             </form>
